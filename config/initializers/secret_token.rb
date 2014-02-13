35e905ec9de11b7f8b97a3ead178e1c23812cb3a
Andy Maes <andy@kabisa.nl>
2014-02-13 17:11:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashCardQuiz::Application.config.secret_key_base = '704eb5619cdd57a0023993245bcb36f8396c6660a5e47d5ec34b62172953e0be0cf8e091ca56adf25334c6b3b1e1c9b3bcf2aaaf4ca28fd27318d3c9f82c89f7'
